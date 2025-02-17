cd347b3193d29953f51a22b9af8d68366267b8b6
Zealotzz <125241292+Zealotzz@users.noreply.github.com>
2025-02-18 02:55:54 +0800

    inital color+ fonts + classes + metadata

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,6 +10,16 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        lightGray: "var(--lightGray)",
+        darkGray: "var(--darkGray)",
+        mintBreeze: "var(--mintBreeze)",
+        tropicalTeal: "var(--tropicalTeal)",
+        verdantWave: "var(--verdantWave)",
+        forestCanopy: "var(--forestCanopy)",
+        midnightLagoon: "var(--midnightLagoon)",
+      },
+      fontFamily: {
+        code: ["SourceCodePro", "Montserrat", "Arial"],
       },
     },
   },
