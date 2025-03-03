15fb4306bb3452b1d4f7cb64109a22a7a2cdc09d
Zealotzz <125241292+Zealotzz@users.noreply.github.com>
2025-03-04 02:17:20 +0800

    Made logo link to home page

--- src/app/navbar.js
+++ src/app/navbar.js
@@ -26,10 +26,12 @@ export default function Navbar({ pageIndex = 0, navState = 0 }) {
     <>
       <div id="navbar" className="fixed top-0 w-full bg-background px-5 sm:px-9 py-2 opacity-0 transition-opacity duration-200 z-50">
         <div className="flex mx-auto max-w-7xl w-full justify-between">
-          <div className="flex items-center gap-3">
-            <Logo size={24} logoColor="white" />
-            <p className="text-lg">Royce's Portfolio</p>
-          </div>
+          <Link href="/">
+            <div className="flex items-center gap-3">
+              <Logo size={24} logoColor="white" />
+              <p className="text-lg">Royce's Portfolio</p>
+            </div>
+          </Link>
           <div className="flex items-center gap-6">
             <Link href="/" className={`text-lg ${pageIndex == 0 ? "underline" : ""}`}>About</Link>
             <Link href="/projects" className={`text-lg ${pageIndex == 1 ? "underline" : ""}`}>Projects</Link>
